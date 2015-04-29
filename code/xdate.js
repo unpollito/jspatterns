cfaef6719d427f3a05af757fb65bffc27223c026
David Martín-González <martingonzalezdavid@gmail.com>
2015-04-29 20:21:31 +0200

    Implemented diff operations

--- code/xdate.js
+++ code/xdate.js
@@ -99,8 +99,53 @@ function xDate(spec) {
 		1     //
 	];
 
+	// Adders
+	function add(xdate, fieldIndex, delta, preventOverflow) {
+		delta = Number(delta);
+		var intDelta = Math.floor(delta);
+		xdate['set' + methodSubjects[fieldIndex]](
+			xdate['get' + methodSubjects[fieldIndex]]() + intDelta,
+			preventOverflow || false
+		);
+		if (intDelta != delta && fieldIndex < MILLISECONDS) {
+			add(xdate, fieldIndex+1, (delta-intDelta)*unitsWithin[fieldIndex], preventOverflow);
+		}
+	}
+
+	// Differs
+	var diff = function(xdate1, xdate2, fieldIndex) { // fieldIndex=FULLYEAR is for years, fieldIndex=DATE is for days
+		var old_utc_1 = xdate1.getUTCMode(), old_utc_2 = xdate2.getUTCMode();
+		xdate1.setUTCMode(true, true);
+		xdate2.setUTCMode(true, true);
+		var v = 0;
+		if (fieldIndex == FULLYEAR || fieldIndex == MONTH) {
+			for (var i=MILLISECONDS, methodName; i>=fieldIndex; i--) {
+				v /= unitsWithin[i];
+				v += xdate2['get' + methodSubjects[i]]() - xdate1['get' + methodSubjects[i]]();
+			}
+			if (fieldIndex == MONTH) {
+				v += (xdate2.getFullYear() - xdate1.getFullYear()) * 12;
+			}
+		}
+		else if (fieldIndex == DATE) {
+			var clear1 = xdate1.toDate().setUTCHours(0, 0, 0, 0); // returns an ms value
+			var clear2 = xdate2.toDate().setUTCHours(0, 0, 0, 0); // returns an ms value
+			v = Math.round((clear2 - clear1) / DAY_MS) + ((xdate2 - clear2) - (xdate1 - clear1)) / DAY_MS;
+		}
+		else {
+			v = (xdate2 - xdate1) / [
+					3600000, // milliseconds in hour
+					60000,   // milliseconds in minute
+					1000,    // milliseconds in second
+					1        //
+				][fieldIndex - 3];
+		}
+		xdate1.setUTCMode(old_utc_1);
+		xdate2.setUTCMode(old_utc_2);
+		return v;
+	};
+
 	// Actual implementation
-	var utcMode = spec.utcMode === true;
 	if (spec.date)
 	{
 		that = new Date(spec.date);
@@ -125,8 +170,19 @@ function xDate(spec) {
 		that = parseISO(spec.datestring, utcMode);
 	}
 
-	// Each setter returns "that" to allow for chaining
-	for (var i = 0; i < methodSubjects.length; i++)
+	var utcMode = spec.utcMode === true;
+	that.getUTCMode = function()
+	{
+		return utcMode;
+	};
+	that.setUTCMode = function(val)
+	{
+		utcMode = val === true;
+		return that;
+	};
+
+	// Each setter returns "that" to allow for chaining. Do not define setFullYear and setMonth yet.
+	for (var i = 2; i < methodSubjects.length; i++)
 	{
 		var field = methodSubjects[i];
 		var setter_name = 'set' + field;
@@ -164,19 +220,6 @@ function xDate(spec) {
 		return that;
 	};
 
-	// Adders
-	function add(xdate, fieldIndex, delta, preventOverflow) {
-		delta = Number(delta);
-		var intDelta = Math.floor(delta);
-		xdate['set' + methodSubjects[fieldIndex]](
-			xdate['get' + methodSubjects[fieldIndex]]() + intDelta,
-			preventOverflow || false
-		);
-		if (intDelta != delta && fieldIndex < MILLISECONDS) {
-			add(xdate, fieldIndex+1, (delta-intDelta)*unitsWithin[fieldIndex], preventOverflow);
-		}
-	}
-
 	for (var i = 0; i < methodSubjects.length; i++)
 	{
 		if (i <= MILLISECONDS)
@@ -188,6 +231,10 @@ function xDate(spec) {
 				that['add' + plural] = function(delta, preventOverflow) {
 					add(this, i, delta, preventOverflow);
 					return this;
+				};
+				that['diff' + plural] = function(otherDate)
+				{
+					return diff(this, otherDate, i);
 				};
 			})(i);
 		}
